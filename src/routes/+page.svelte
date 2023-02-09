7d0849e0326b8f9ba4fb79b56ff72c8120af0dae
Anirudh <aswalanirudh@outlook.in>
2023-02-09 18:34:49 +0530

    init

---
+++ src/routes/+page.svelte
@@ -0,0 +1,193 @@
+<script>
+    function login() {}
+    let imgPicker = {
+        1: "assets/pink.png",
+        2: "assets/plant.jpg",
+        3: "assets/yellow.png",
+    };
+
+    let colorPicker = {
+        1: "#d43d79",
+        2: "#238940",
+        3: "#BB7527",
+    };
+
+    const choice = Math.floor(Math.random() * 3) + 1;
+    const image = imgPicker[choice];
+    const colour = colorPicker[choice];
+</script>
+
+<body style="--main-accent-color: {colour}">
+    <div class="layout">
+        <div class="header">
+            <h1>MOORSEE</h1>
+            <p>.._ _. _.. . ._. ._.. .. _. .</p>
+        </div>
+        <div class="landing">
+            <div class="info">
+                <p id="fact-txt">
+                    85% of plant life is found in oceans. You only have 15% to
+                    take care of.
+                </p>
+                <p id="cringe-txt">
+                    Lorem ipsum dolor sit amet <br />Lorem ipsum dolor
+                </p>
+                <div class="input-field">
+                    <input
+                        class="inp-f"
+                        type="text"
+                        name="name-field"
+                        id="name-field"
+                        placeholder="Name"
+                    />
+                    <input
+                        class="inp-f"
+                        type="password"
+                        name="secret-field"
+                        id="secret-field"
+                        placeholder="Secret"
+                    />
+                </div>
+            </div>
+            <div class="img-holder">
+                <img src={image} alt="pink" on:click={login} />
+
+                <p>Picture by peron person</p>
+            </div>
+        </div>
+    </div>
+</body>
+
+<style>
+    @import url("https://fonts.googleapis.com/css2?family=Port+Lligat+Slab&family=Sacramento&family=VT323&display=swap");
+
+    :root {
+        --main-bg-color: #f2f2ed;
+        /* --main-accent-color: #ffffff; */
+        --secondary-tet-color: rgb(98, 98, 98);
+    }
+
+    * {
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+    }
+
+    body {
+        background-color: var(--main-bg-color);
+    }
+
+    .layout {
+        /* margin-top: 4rem; */
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        margin-left: 16rem;
+        margin-right: 16rem;
+        height: 100vh;
+    }
+
+    .header {
+        margin-bottom: 3rem;
+        font-size: 62px;
+    }
+
+    .header p {
+        font-family: "VT323", monospace;
+        font-size: 0.25em;
+    }
+
+    .header h1 {
+        font-family: "VT323", monospace;
+        margin: 0px;
+        font-size: 1em;
+        margin-bottom: -20px;
+    }
+
+    .landing {
+        display: flex;
+        height: 400px;
+    }
+
+    .info {
+        width: 70%;
+        height: auto;
+        margin-top: 4rem;
+        margin-bottom: 4rem;
+        padding-right: 6rem;
+        margin-right: auto;
+    }
+
+    #fact-txt {
+        font-family: "Sacramento", cursive;
+        font-size: 2.5em;
+        margin-bottom: 2rem;
+        color: var(--main-accent-color);
+    }
+
+    #cringe-txt {
+        font-family: "VT323", monospace;
+        font-size: 1em;
+        margin-bottom: 2rem;
+        color: var(--secondary-tet-color);
+        width: 50%;
+    }
+
+    .img-holder {
+        height: 375px;
+        width: 25%;
+    }
+
+    .img-holder img {
+        height: calc(100% - 20px);
+        /* height: 100%; */
+        width: 100%;
+        object-fit: cover;
+        border-radius: 4px;
+    }
+
+    .img-holder img:hover {
+        cursor: pointer;
+    }
+
+    .img-holder p {
+        font-family: "Port Lligat Slab", serif;
+        font-size: 1em;
+        color: var(--secondary-tet-color);
+        /* align this to the center of .img-holder */
+        width: 100%;
+        text-align: center;
+    }
+
+    .input-field {
+        display: flex;
+    }
+
+    .inp-f {
+        background: none;
+        color: var(--main-accent-color);
+        font-family: "VT323", monospace;
+        font-size: 1em;
+        padding: 0.5rem 1rem;
+        border-radius: 5px;
+        border: 3px dashed black;
+        margin-right: 1rem;
+        cursor: pointer;
+        text-align: center;
+    }
+
+    .inp-f::placeholder {
+        color: #000000;
+        font-family: "VT323", monospace;
+        font-size: 1em;
+    }
+
+    .inp-f:hover {
+        border: 3px solid var(--main-accent-color);
+    }
+
+    .inp-f:focus {
+        outline: none;
+        border: 3px solid var(--main-accent-color);
+    }
+</style>
---
+++ src/routes/home/+page.svelte
@@ -0,0 +1,149 @@
+<script>
+</script>
+
+<body>
+    <div class="header">
+        <h1>MOORSEE</h1>
+        <p>.._ _. _.. . ._. ._.. .. _. .</p>
+    </div>
+    <div class="spacer" />
+    <div class="container">
+        <div
+            id="editor"
+            contenteditable="true"
+            default-text="true"
+            oninput="checkPlaceholder()"
+        />
+        <div class="buttons">
+            <div class="editor-btn">abc</div>
+            <div class="editor-btn">dot</div>
+            <div class="editor-btn">dash</div>
+            <div class="editor-btn">change</div>
+        </div>
+    </div>
+
+    <div class="nav">
+        <div class="nav-btn">home</div>
+        <div class="nav-btn">about</div>
+        <div class="nav-btn">contact</div>
+    </div>
+</body>
+
+<!-- <script>
+    function checkPlaceholder() {
+        // if (document.getElementById("editor").innerHTML === "") {
+        //     document.getElementById("editor").innerHTML = "Placeholder Text";
+        // }
+        document.getElementById("editor").setAttribute("default-text", "false");
+    }
+    checkPlaceholder()
+</script> -->
+<style>
+    @import url("https://fonts.googleapis.com/css2?family=Port+Lligat+Slab&family=Sacramento&family=VT323&display=swap");
+
+    :root {
+        --main-bg-color: #f2f2ed;
+        --main-accent-color: #d43d7950;
+        --secondary-tet-color: rgb(98, 98, 98);
+        --test-colours: #dfdfdfc0;
+    }
+
+    body {
+        background-color: var(--main-bg-color);
+        min-height: 100vh;
+        display: flex;
+        flex-direction: column;
+    }
+
+    .header {
+        font-family: "VT323", monospace;
+        /* width: 100vw; */
+        padding-left: 125px;
+        padding-top: 3rem;
+        font-size: 16px;
+        /* background: var(--test-colours); */
+    }
+
+    .header h1 {
+        font-family: "VT323", monospace;
+        margin: 0px;
+        margin-bottom: -12px;
+    }
+
+    .header p {
+        font-family: "VT323", monospace;
+        margin: 0px;
+        font-size: 0.5em;
+    }
+
+    .container {
+        display: flex;
+        /* flex-grow: 1; */
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+        /* height: 100vh; */
+        width: 64vw;
+        margin-left: auto;
+        margin-right: auto;
+        /* margin-top: auto; */
+    }
+
+    .buttons {
+        display: flex;
+        justify-content: space-around;
+        width: 64%;
+        margin-left: auto;
+        margin-right: auto;
+    }
+
+    .editor-btn {
+        /* margin-right: 1rem;
+        margin-left: 1rem;
+        margin-top: 1rem;
+        margin-bottom: 1rem; */
+        padding: 0.5rem;
+        /* border: 1px solid var(--main-accent-color); */
+        border-radius: 5px;
+        /* background: var(--test-colours); */
+        font-family: "VT323", monospace;
+        font-size: 1.5em;
+        color: var(--main-accent-color);
+    }
+
+    #editor {
+        display: flex;
+        height: 150px;
+        width: 100%;
+        flex-direction: column;
+        justify-content: center;
+        text-align: center;
+        overflow-y: scroll;
+        overflow-x: visible;
+        word-wrap: break-word;
+        margin-bottom: 1.5rem;
+        font-family: "VT323", monospace;
+        padding: 0.5rem;
+        font-size: 1.25em;
+        border: 2px dashed black;
+        border-radius: 0.5rem;
+        outline: 0px;
+    }
+
+    #editor:hover {
+        border: 2px dashed var(--main-accent-color);
+    }
+    #editor:focus {
+        border: 2px dashed var(--main-accent-color);
+    }
+    .nav {
+        display: flex;
+        justify-content: space-around;
+        width: 50%;
+        margin-left: auto;
+        margin-right: auto;
+    }
+    .spacer {
+        height: 150px;
+    }
+</style>
