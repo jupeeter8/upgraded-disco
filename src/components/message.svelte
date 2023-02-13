50fb4570c9310a721c65b29c9295b9b4800d8e63
Anirudh <aswalanirudh@outlook.in>
2023-02-13 12:13:59 +0530

    Feat: Created Message component

---
+++ src/components/message.svelte
@@ -0,0 +1,56 @@
+<script>
+</script>
+
+<div class="msg-f">
+    <div class="icn" />
+    <div class="msg-content">
+        Lorem Ipsum nice guchi hello amoug us Lorem Ipsum nice guchi hello amoug
+        us
+    </div>
+    <div class="icn-sq" />
+    <div class="date">Feb 12 23</div>
+</div>
+
+<style>
+    .msg-f {
+        display: flex;
+        margin: 0%;
+        align-items: center;
+        padding-left: 1em;
+        padding-right: 1em;
+        padding-top: 0.5em;
+        margin-bottom: 0.5em;
+    }
+    .msg-content {
+        padding-left: 0.2em;
+        padding-right: 0.2em;
+        font-family: "Sacramento", cursive;
+        font-size: 1.75rem;
+        width: 75%;
+        white-space: nowrap;
+        overflow: hidden;
+        text-overflow: ellipsis;
+    }
+    .icn {
+        width: 0.5rem;
+        height: 0.5rem;
+        border-radius: 50%;
+        margin-right: 0.75em;
+        background-color: var(--main-accent-color);
+        font-weight: bold;
+        margin-left: 2em;
+    }
+    .icn-sq {
+        width: 0.4rem;
+        height: 0.4rem;
+        border-radius: 2px;
+        background-color: black;
+        margin-left: 1.5em;
+        margin-right: 0.8em;
+        font-weight: bold;
+    }
+    .date {
+        font-family: "VT323", monospace;
+        font-size: 1.5rem;
+    }
+</style>
