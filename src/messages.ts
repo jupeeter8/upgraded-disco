090cba73a934e23051062f1c28708325cf15b73a
Anirudh <aswalanirudh@outlook.in>
2023-02-16 11:17:39 +0530

    Moved Logic to service folder

--- src/messages.ts
+++
@@ -1,6 +0,0 @@
-import { getFirestore, collection, getDoc, doc, setDoc } from "firebase/firestore"
-import { app } from "./firebase"
-
-export const db = getFirestore(app)
-
-export { getDoc, collection, doc, setDoc }
