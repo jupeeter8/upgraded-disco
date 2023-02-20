544ff0745d45511cc98fac9f1153a3609af2fc8f
Anirudh <aswalanirudh@outlook.in>
2023-02-20 19:26:42 +0530

    functionality to change theme

--- src/routes/account/+page.svelte
+++ src/routes/account/+page.svelte
@@ -1,16 +1,45 @@
 <script>
-    import Header from "../../components/Header.svelte";
     import { goto } from "$app/navigation";
+    import { changeCollection, changeTheme, theme } from "../../service/theme";
+    let themeVal;
+    theme.subscribe((value) => {
+        themeVal = value;
+    });
+    function change() {
+        changeCollection();
+        changeTheme();
+        localStorage.setItem("colour", themeVal.color);
+    }
+    function changetheme() {
+        changeTheme();
+        localStorage.setItem("colour", themeVal.color);
+    }
 
     const navigate = () => goto("/home");
 </script>
 
-<div class="body">
-    <div class="header" on:click={navigate} on:keyup={() => {}}>
+<div
+    class="body"
+    style="--main-accent-color: {themeVal.color}; --sec-a-color: {themeVal.color +
+        '80'}"
+>
+    <div class="header">
         <h1>MOORSEE</h1>
         <p>.._ _. _.. . ._. ._.. .. _. .</p>
     </div>
-    <div class="img" />
+    <div class="img-holder">
+        <img
+            src={themeVal.path}
+            alt="wow"
+            on:click={navigate}
+            on:keyup={() => {}}
+        />
+    </div>
+    <a href={themeVal.link}>
+        <p id="img-info">{themeVal.name} by {themeVal.artist}</p>
+    </a>
+    <button on:click={change}>Change</button>
+    <button on:click={changetheme}>Change Theme</button>
 </div>
 
 <style>
@@ -18,6 +47,7 @@
     :global(body) {
         margin: 0;
         padding: 0;
+        background-color: #f2f2ed;
     }
     .body {
         display: flex;
@@ -29,19 +59,58 @@
     .header {
         font-family: "VT323", monospace;
         font-size: 32px;
-        width: fit-content;
         margin-top: 3rem;
+        width: 100%;
+        margin-bottom: 1rem;
+        user-select: none;
     }
 
     .header h1 {
         margin: 0px;
         margin-bottom: -24px;
-        cursor: pointer;
+        width: fit-content;
+        margin-left: auto;
+        margin-right: auto;
     }
 
     .header p {
         margin: 0px;
         font-size: 0.5em;
+        width: fit-content;
+        margin-left: auto;
+        margin-right: auto;
+    }
+    .img-holder {
+        height: 375px;
+        width: 18%;
+        margin-left: auto;
+        margin-right: auto;
+        margin-bottom: 0.5em;
+    }
+
+    .img-holder img {
+        height: calc(100% - 5px);
+        width: 100%;
+        object-fit: cover;
+        border-radius: 4px;
+    }
+
+    .img-holder img:hover {
         cursor: pointer;
+    }
+    a {
+        width: 100vw;
+        /* remove underline */
+        text-decoration: none;
+    }
+    #img-info {
+        font-family: "Port Lligat Slab", serif;
+        font-size: 0.8em;
+        color: var(--sec-a-color);
+        text-align: center;
+        margin: 0px;
+        width: 18%;
+        margin-left: auto;
+        margin-right: auto;
     }
 </style>
