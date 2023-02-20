68428b99b806a88c6b3b7a7e5cffdea750ba0f17
Anirudh <aswalanirudh@outlook.in>
2023-02-20 12:55:52 +0530

    added Navigation to Header

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -1,12 +1,15 @@
 <script>
+    import { goto } from "$app/navigation";
+    const navigate = () => goto("/account");
 </script>
 
-<div class="header">
+<div class="header" on:click={navigate}>
     <h1>MOORSEE</h1>
     <p>.._ _. _.. . ._. ._.. .. _. .</p>
 </div>
 
 <style>
+    @import url("https://fonts.googleapis.com/css2?family=VT323&display=swap");
     .header {
         font-family: "VT323", monospace;
         padding-left: 125px;
@@ -17,10 +20,12 @@
     .header h1 {
         margin: 0px;
         margin-bottom: -12px;
+        cursor: pointer;
     }
 
     .header p {
         margin: 0px;
         font-size: 0.5em;
+        cursor: pointer;
     }
 </style>
